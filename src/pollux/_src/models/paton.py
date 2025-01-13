7ed02701125dd3dcbbb40419897a1c01396ebe4d
Adrian Price-Whelan <adrian.prw@gmail.com>
2025-01-13 12:45:30 -0500

    moving common functionality up to shared class

--- src/pollux/_src/models/paton.py
+++ src/pollux/_src/models/paton.py
@@ -1,32 +1,27 @@
 from typing import Any
 
-import equinox as eqx
 import jax
-import jax.numpy as jnp
 import numpyro
 import numpyro.distributions as dist
 from jax.typing import ArrayLike
 
-from ..exceptions import ModelValidationError
-from ..shared import Output
-from ..typing import TransformParamsT, TransformT
+from ..shared import AbstractModel
 
 
-class Paton(eqx.Module):
+class Paton(AbstractModel):
+    """The Paton is a generative model for stellar spectra, labels, and other data.
+
+    Parameters
+    ----------
+    latent_size
+        The size of the latent vectors for the model.
+
+    Examples
+    --------
+
+    """
+
     latent_size: int
-    outputs: dict[str, Output] = eqx.field(default_factory=dict)
-
-    def register_output(
-        self,
-        name: str,
-        size: int,
-        transform: TransformT,
-        transform_params: TransformParamsT,
-    ) -> None:
-        """Register a new output with its transform and parameter priors."""
-        self.outputs[name] = Output(
-            size=size, transform=transform, transform_params=transform_params
-        )
 
     def predict_outputs(
         self,
@@ -66,19 +61,6 @@ class Paton(eqx.Module):
             results[name] = output.transform(latent_z, *output_params)
 
         return results
-
-    def _validate_data(
-        self, data: dict[str, ArrayLike], errs: dict[str, ArrayLike]
-    ) -> tuple[dict[str, jax.Array], dict[str, jax.Array]]:
-        """Validate data and errors, returning as jax arrays."""
-        # check that data and errs have the same keys
-        if set(data.keys()) != set(errs.keys()):
-            msg = "Data and errors must have the same keys"
-            raise ModelValidationError(msg)
-
-        _data = {k: jnp.array(v) for k, v in data.items()}
-        _errs = {k: jnp.array(v) for k, v in errs.items()}
-        return _data, _errs
 
     def setup_numpyro(
         self,
@@ -164,70 +146,3 @@ class Paton(eqx.Module):
                 sample_shape=(n_data,),
             )
             self.setup_numpyro(latent_z, data_, errs_)  # type: ignore[arg-type]
-
-    def unpack_numpyro_params(
-        self, params: dict[str, jax.Array]
-    ) -> dict[str, dict[str, jax.Array]]:
-        """Unpack numpyro parameters into a nested structure.
-
-        numpyro parameters use names like "output_name:param_name" to make the numpyro
-        internal names unique. However, this method unpacks these into a nested
-        dictionary keyed on [output_name][param_name].
-
-        Parameters
-        ----------
-        params
-            A dictionary of numpyro parameters. The keys should be in the format
-            "output_name:param_name".
-
-        Returns
-        -------
-        dict
-            A nested dictionary of parameters, where the top level keys are the output
-            names.
-        """
-        unpacked: dict[str, Any | dict[str, Any]] = {}
-        handled_params = set()
-        for name, output in self.outputs.items():
-            unpacked[name] = {}
-            for k in output.transform_params:
-                numpyro_name = f"{name}:{k}"
-                unpacked[name][k] = params[numpyro_name]
-                handled_params.add(numpyro_name)
-
-        for name in set(params.keys()) - handled_params:
-            unpacked[name] = params[name]
-
-        return unpacked
-
-    def pack_numpyro_params(
-        self, params: dict[str, dict[str, jax.Array]]
-    ) -> dict[str, jax.Array]:
-        """Pack parameters into a flat dictionary keyed on numpyro names.
-
-        This method is the inverse of `unpack_numpyro_params`. It takes a nested
-        dictionary of parameters and flattens it into a dictionary of numpyro
-        parameters. For example, it takes a nested dictionary keyed like
-        [output_name][param_name] and flattens it into a dictionary keyed like
-        "output_name:param_name".
-
-        Parameters
-        ----------
-        params
-            A nested dictionary of parameters, where the top level keys are the output
-            names.
-
-        Returns
-        -------
-        dict
-            A dictionary of numpyro parameters. The keys are in the format
-            "output_name:param_name
-
-        """
-        packed: dict[str, jax.Array] = {}
-        for name, output in self.outputs.items():
-            for k in output.transform_params:
-                numpyro_name = f"{name}:{k}"
-                packed[numpyro_name] = params[name][k]
-
-        return packed
